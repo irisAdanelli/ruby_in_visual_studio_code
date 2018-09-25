9c0a8e18fb7ceaf0acccedcffc28ac45691019f0
Iris Adanelli Lopez Garcia <36413206+irisAdanelli@users.noreply.github.com>
2018-09-25 09:58:56 -0600

    Create nombre_falsos.rb

---
+++ nombre_falsos.rb
@@ -0,0 +1,8 @@
+require "faker"
+
+puts Faker::Name.name     
+puts Faker::Internet.email   
+
+#puts "Nombre: #{name} , EMAIL: #{email}"
+
+# PENDIENTE: gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
